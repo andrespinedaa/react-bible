e516481b70cf1cc012772ed7f39c7745a18fd9f6
Andres Pineda <andrespupepine@gmail.com>
2024-08-16 21:55:29 -0500

    VerseText Component analiza si el text tiene saltos de linea (\n) 0 si tiene referencias (@) eliminando el custom hook de useNewLine si no tiene referencias ni saltos de linea retorna el verse.text o text

--- src/components/@verse/verseBody/VerseText.tsx
+++ src/components/@verse/verseBody/VerseText.tsx
@@ -1,20 +1,38 @@
-import { useNewLine } from "../../../hooks";
-import { useVerseBodyContext, useVersesContext } from "../../@contexts";
+import _ from "lodash";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useVerseBodyContext } from "../../@contexts";
+import VerseReference from "../verseReference/VerseReference";
 
 /* HACER UN REGEX PARA CAPTURAR LAS PALARBAS DE JESUS */
 
 interface VerseTextProps {
-  text?: string;
-  jesusWords?: boolean;
+	text?: string;
+	jesusWords?: boolean;
+	children?: React.ReactElement<typeof VerseReference>;
 }
 
-function VerseText({ text = undefined }: VerseTextProps) {
-  const { psalmStyle } = useVersesContext();
-  const { verse } = useVerseBodyContext();
-  const innerText = text ?? verse.text;
-  const textRender = psalmStyle ? useNewLine(innerText) : innerText;
+function VerseText({ text = undefined, children }: VerseTextProps) {
+	const { verse } = useVerseBodyContext();
+	const innerText = text ?? verse.text;
 
-  return textRender;
+	if (!innerText.includes("\n") && !innerText.includes("@")) return innerText;
+
+	return _.map(_.split(verse.text, "\n"), (jumpLine, _index2, partList) => {
+		return (
+			<React.Fragment key={uuidv4()}>
+				{_.map(_.split(jumpLine, "@"), (lineAt, index, partsLineAt) => {
+					return (
+						<React.Fragment key={uuidv4()}>
+							{lineAt}
+							{index < partsLineAt.length - 1 && children}
+						</React.Fragment>
+					);
+				})}
+				{partList.length > 1 && <br />}
+			</React.Fragment>
+		);
+	});
 }
 
 export default VerseText;
