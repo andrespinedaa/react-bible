91e8d986d220a6242e6fbf38d36b588a4d663375
Andres Pineda <andrespupepine@gmail.com>
2024-08-17 12:42:21 -0500

    Modificado el barrel de @verse donde se añade VerseReference, modificado el barrel de hooks donde se elimina el  useNewLine y añadido un nuevo barrel en la carpeta verseReference que exporta el componente VerseReference y VerseReference.stories

---
+++ src/components/@verse/verseReference/index.ts
@@ -0,0 +1,3 @@
+export { default as ReferenceScreen } from './ReferenceScreen';
+export { default as VerseReferenceStories } from './VerseReference.stories';
+export { default as VerseReference } from './VerseReference';
