961595d521a418b58df9ca2d4903c17512e6eb0e
Andres Pineda <andrespupepine@gmail.com>
2024-08-19 20:53:00 -0500

    Se refactorizo el VerseComponent, se añadio la funcions numberToLetter, y consume useVerseBodyContext para enumerar o darle una letra a las referencias en linea

--- src/components/@verse/verseReference/VerseReference.tsx
+++ src/components/@verse/verseReference/VerseReference.tsx
@@ -1,42 +1,41 @@
-import React from "react";
 import styled from "styled-components";
+import { useVerseBodyContext } from "../../@contexts";
 
 interface VerseReferenceProps {
-	referenceFormat?: "uppercase" | "lowercase";
-	referenceType?: "letter" | "number" | "romanNumber";
-	hookTagsType?: "parenthesis" | "brackets" | "none";
-	hookType?: "span" | "sup" | "sub";
+  referenceFormat?: "uppercase" | "lowercase";
+  referenceType?: "letter" | "number" | "romanNumber";
+  hookTagsType?: "parenthesis" | "brackets" | "none";
+  hookType?: "span" | "sup" | "sub";
 }
 const StyledVerseReference = styled.span<{
-	$referenceFormat?: "uppercase" | "lowercase" | "capitalize" | "none";
+  $referenceFormat?: "uppercase" | "lowercase" | "capitalize" | "none";
 }>`
-	text-transform: ${(props) => props.$referenceFormat};
+  text-transform: ${(props) => props.$referenceFormat};
 `;
 const StyledHook = styled.span``;
 
-function VerseReference({
-	referenceFormat = "uppercase",
-	hookTagsType = "parenthesis",
-	hookType = "span",
-}: VerseReferenceProps) {
-	const [show, setShow] = React.useState<boolean>(false);
-	const handleMouseOver = () => setShow(true);
+const numberToLetter = (num: number) => {
+  return String.fromCharCode(65 + (num % 26));
+};
 
-	return (
-		<StyledVerseReference
-			onMouseOver={() => handleMouseOver()}
-			$referenceFormat={`${referenceFormat}`}
-		>
-			<StyledHook as={hookType}>
-				{hookTagsType === "parenthesis"
-					? "(a)"
-					: hookTagsType === "brackets"
-						? "[a]"
-						: hookTagsType === "none" && "a"}
-			</StyledHook>
-			{show && <span></span>}
-		</StyledVerseReference>
-	);
+function VerseReference({
+  referenceFormat = "lowercase",
+  hookTagsType = "parenthesis",
+  referenceType = "letter",
+  hookType = "span",
+}: VerseReferenceProps) {
+  const { refAt } = useVerseBodyContext();
+  return (
+    <StyledVerseReference $referenceFormat={`${referenceFormat}`}>
+      <StyledHook as={hookType}>
+        {hookTagsType === "parenthesis"
+          ? `(${referenceType === "number" ? refAt.current : referenceType === "letter" && numberToLetter(refAt.current)})`
+          : hookTagsType === "brackets"
+            ? `[${referenceType === "number" ? refAt.current : referenceType === "letter" && numberToLetter(refAt.current)}}]`
+            : hookTagsType === "none" && "a"}
+      </StyledHook>
+    </StyledVerseReference>
+  );
 }
 
 export default VerseReference;
