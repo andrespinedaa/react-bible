e6d9890a12b21f1e75ff3bc14171a07cd3ac85ea
Andres Pineda <andrespupepine@gmail.com>
2024-08-21 15:48:01 -0500

    Se elimino la prop 'firstNumberBig' ahora esta se añade desde VerseNumber, en StyledVerses la propiedad display será flex, se elimino el VerseProvider, ahora renderizara sus children entre Verse.tsx que contiene el VerseProvider

--- src/components/@verse/Verses.tsx
+++ src/components/@verse/Verses.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
 import { useBible } from "../../hooks/useBible";
 import { paragraph } from "../../utilities";
-import { VerseProvider, VersesProvider } from "../@contexts";
+import { VersesProvider } from "../@contexts";
 
 export interface VersesProps extends React.ComponentPropsWithRef<"div"> {
-  firstNumberBig?: boolean;
   alignText?: "center" | "left" | "right";
   paragraphs?: paragraph[];
   separateParagraphs?: boolean;
@@ -19,22 +17,22 @@ const StyledVerses = styled.div<{
   $alignText?: "center" | "left" | "right";
   $separateParagraphs?: boolean;
 }>`
-  display: ${(props) => (props.$separateParagraphs ? "flex" : "inline")};
+  display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   font-size: ${(props) =>
     props.theme.bibleSizes.VerseTextSize[props.theme.defaultSize]};
   text-align: ${(props) => props.$alignText};
-  gap: ${(props) => props.theme.bibleVerses.spaceBetweenVerses};
+  gap: ${(props) =>
+    props.$separateParagraphs && props.theme.bibleVerses.spaceBetweenVerses};
 `;
 
 function Verses({
-  paragraphs = undefined,
-  firstNumberBig = true,
-  alignText = "left",
   separateParagraphs = true,
   separateVerses = false,
+  paragraphs = undefined,
   psalmStyle = true,
+  alignText = "left",
   children,
   ...restProps
 }: VersesProps) {
@@ -50,25 +48,12 @@ function Verses({
       <VersesProvider
         value={{
           alignText,
-          firstNumberBig,
           psalmStyle,
           separateVerses,
+          paragraphs: innerParagraphs,
         }}
       >
-        {innerParagraphs.map((paragraph) => {
-          return (
-            <VerseProvider
-              key={uuidv4()}
-              value={{
-                paragraph,
-                subTitle: paragraph.subTitle,
-                title: paragraph.title,
-              }}
-            >
-              {children}
-            </VerseProvider>
-          );
-        })}
+        {children}
       </VersesProvider>
     </StyledVerses>
   );
