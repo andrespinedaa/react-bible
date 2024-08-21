853cb5334655b47a72836214057e4b706afe9549
Andres Pineda <andrespupepine@gmail.com>
2024-08-20 23:19:32 -0500

    Se actualizo la VerseReferenceStoy

--- src/components/@verse/verseReference/VerseReference.stories.tsx
+++ src/components/@verse/verseReference/VerseReference.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import VerseReference from "./VerseReference";
 import { VerseBody, VerseNumber, VerseText } from "../verseBody";
-import { VersesBox } from "../Verses.stories";
 import Verses from "../Verses";
 
 type VerseReferenceProps = React.ComponentProps<typeof VerseReference>;
@@ -17,8 +16,13 @@ const meta: Meta<VerseReferenceProps> = {
     },
     referenceType: {
       control: "inline-radio",
-      options: ["letter", "number", "romanNumber"],
+      options: ["letter", "number"],
       description: "Reference type",
+    },
+    referenceStyle: {
+      control: "inline-radio",
+      options: ["none", "bold", "italic", "boldAndItalic"],
+      description: "Reference style",
     },
     hookTagsType: {
       control: "inline-radio",
@@ -38,14 +42,31 @@ export default meta;
 type Story = StoryObj<VerseReferenceProps>;
 
 export const VerseReferenceBox: Story = {
+  name: "Verse Reference",
   args: {
     referenceFormat: "uppercase",
+    referenceStyle: "none",
     referenceType: "letter",
     hookTagsType: "parenthesis",
     hookType: "span",
   },
   render: (args) => (
-    <Verses {...VersesBox.args}>
+    <Verses
+      verses={[
+        {
+          verses: [
+            {
+              text: `Estaba entonces enfermo uno llamado Lázaro, de Betania, la aldea de María y de Marta su hermana.@`,
+              number: 1,
+            },
+            {
+              text: "(María, cuyo hermano Lázaro estaba enfermo, fue la que ungió al Señor con perfume, y le enjugó los pies con sus cabellos.@)",
+              number: 2,
+            },
+          ],
+        },
+      ]}
+    >
       <VerseBody>
         <VerseNumber />
         <VerseText>
